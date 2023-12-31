766d2d576218863d8fc016cd9f9c715ddf3ecfdd
Arpit Baheti <87475705+arpitbaheti2002@users.noreply.github.com>
2023-12-31 20:42:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Resume from "./components/VIT_Resume/Resume";
+
+function App() {
+  return (
+    <div className="App">
+     <Resume />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/VIT_Resume/Resume.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Resume() {
+  return (
+    <div className='a4-page resume'>
+      
+    </div>
+  )
+}
+
+export default Resume;
