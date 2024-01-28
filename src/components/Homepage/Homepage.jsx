d80f0a03034096f32b68f667496dedb3f9b4e332
Arpit Baheti <87475705+arpitbaheti2002@users.noreply.github.com>
2024-01-28 21:39:36 +0530

    added homepage animations

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,31 +1,124 @@
-// Homepage.jsx
 import React from 'react';
 import './homepage.css';
 import { useNavigate } from 'react-router-dom';
+import { motion } from "framer-motion";
+import Typewriter from 'typewriter-effect';
 
 function Homepage(props) {
     const navigate = useNavigate();
     const handleClick = (link) => {
         navigate(link);
     };
+
+    const subtitle1 = Array.from("Elevate Your Profile Effortlessly");
+    const subtitle2 = Array.from("Edit, Download, VIT Format Ready!");
+
+    const container = {
+        hidden: { opacity: 0 },
+        visible: (i = 1) => ({
+          opacity: 1,
+          transition: { staggerChildren: 0.03, delayChildren: 0.04 * i + 1.5 },
+        }),
+      };
+    
+      const child = {
+        visible: {
+          opacity: 1,
+          transition: {
+            type: "spring",
+            damping: 12,
+            stiffness: 100,
+          },
+        },
+        hidden: {
+          opacity: 0,
+          transition: {
+            type: "spring",
+            damping: 12,
+            stiffness: 100,
+          },
+        },
+      };
+
     return (
-        <div className='homepage'>
+    <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }} 
+        transition={{ duration: 0.5, type: "Inertia" }}
+        className='homepage'
+    >       
         <div className='content-container'>
             <div className='title-section'>
             <div className='home-page-title'>
-                <span className='blue-text'>Resu </span>
-                <span className='black-text'>Mate</span>
+                <motion.span className='blue-text'
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{
+                    duration: 1,
+                    delay: 0.5
+                }}>
+                Resu 
+                </motion.span>
+                <motion.span className='black-text'
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{
+                    duration: 1,
+                    delay: 1
+                }}>
+                Mate
+                </motion.span>
             </div>
+            
             <div className='subtitle'>
-                Elevate Your Profile Effortlessly<br/>Edit, Download, VIT Format Ready!
+            <motion.div
+                style={{ overflow: "hidden", display: "flex", fontSize: "2rem" }}
+                variants={container}
+                initial="hidden"
+                animate="visible"
+                >
+                {subtitle1.map((letter, index) => (
+                    <motion.span variants={child} key={index}>
+                    {letter === " " ? "\u00A0" : letter}
+                    </motion.span>
+                ))}
+            </motion.div>
+            <Typewriter
+                options={{
+                    strings: ['', 'Create, Edit, Download', 'VIT Format Ready'],
+                    autoStart: true,
+                    loop: true,
+                    pauseFor: 2000
+                }}
+            />
+            {/* <motion.div
+                style={{ overflow: "hidden", display: "flex", fontSize: "2rem" }}
+                variants={container}
+                initial="hidden"
+                animate="visible"
+                custom={subtitle1.length}
+                >
+                {subtitle2.map((letter, index) => (
+                    <motion.span variants={child} key={index} custom={subtitle1.length}>
+                    {letter === " " ? "\u00A0" : letter}
+                    </motion.span>
+                ))}
+            </motion.div> */}
+                
             </div>
             </div>
             <div className='feature-section'>
-            <div className='feature-box'>
-            <div className='card' onClick={()=>handleClick('/VITResume')}>
-                <div className='feature-title'>VIT Format</div>
-            </div>
-            </div>
+                <motion.div
+                    initial={{ opacity: 0, y: 50 }} 
+                    animate={{ opacity: 1, y: 0 }} 
+                    transition={{ delay: 4, type: "spring", damping: 20, stiffness: 100 }} 
+                >
+                    <div className='feature-box'>
+                        <div className='card' onClick={()=>handleClick('/VITResume')}>
+                            <div className='feature-title'>VIT Format</div>
+                        </div>
+                    </div>
+                </motion.div>
             {/* <div className='feature-box'>
                 <div className='feature-title'>Auto<br/>Layout</div>
             </div>
@@ -33,11 +126,16 @@ function Homepage(props) {
                 <div className='feature-title'>Create<br/>Now</div>
             </div> */}
             </div>
-            <div className='designer-info'>
+            <motion.div
+                initial={{ opacity: 0, y: 50 }} 
+                animate={{ opacity: 1, y: 0 }} 
+                transition={{ delay: 4, type: "spring", damping: 20, stiffness: 100 }} 
+                className='designer-info'
+            >
                 Brought to you by SCAB.
-            </div>
+            </motion.div>
         </div>
-    </div>
+    </motion.div>
     );
 }
 
